d7a78bd4702c009410637557d96ab9c403e51e0e
Юрий Авдеев <youra.avdeev@MacBook-Pro-Urij-2.local>
2020-03-06 23:01:22 +0300

    макеты писем
    доабвлена верстка писем todo: уточнить данные у РОМЫ  и зашить ссылки

--- public/server.js
+++ public/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const app = express();
-app.use("/", express.static(__dirname ));
+app.use("/", express.static(__dirname));
 
 app.listen(process.env.PORT || "8000", () => {
     console.log("Port: 8000");
