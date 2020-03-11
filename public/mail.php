89308a670f8525770045d68803d99cc421493c98
Юрий Авдеев <youra.avdeev@MacBook-Pro-Urij-2.local>
2020-03-11 21:13:36 +0300

    v1.5.1

--- public/mail.php
+++
@@ -1,8 +0,0 @@
-<?php
-$csvFile = 'user_data.csv';
-$csvData = "{$_POST['surname']}|{$_POST['name']}|{$_POST['surname']}|{$_POST['spec']}|{$_POST['email']}\r\n";// в винде это \r\n вместо просто \n
-echo $csvData;
-$csvData = iconv('UTF-8', 'cp1251', $csvData);
-echo $csvData;
-file_put_contents( $csvFile, $csvData, FILE_APPEND );
-?>
